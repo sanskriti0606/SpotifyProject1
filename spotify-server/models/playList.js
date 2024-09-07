5b9ff361882e3354752644f8b6feac0e8cc382d1
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-09-08 00:09:19 +0530

    Update playList.js

--- spotify-server/models/playList.js
+++ spotify-server/models/playList.js
@@ -3,18 +3,21 @@ import Joi from 'joi';
 
 const playListSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-  songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }], // Correctly reference 'Song'
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  artist: { type: String, required: true }, // Add artist field
+  song: { type: String, required: true },   // Add song URL field
+  image: { type: String },                  // Add image URL field
+  duration: { type: String },               // Add duration field
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 const PlayList = mongoose.model('PlayList', playListSchema);
-
 const validatePlayList = (playList) => {
   const schema = Joi.object({
     name: Joi.string().required(),
-    description: Joi.string().required(),
-    songs: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).optional(), // Validate ObjectId strings
+    artist: Joi.string().required(), // Add artist field
+    song: Joi.string().uri().required(), // Validate URL to the song
+    image: Joi.string().uri().optional(), // Optional image URL
+    duration: Joi.string().pattern(/^(\d+):(\d+)$|^(\d+)$|^(\d+):(\d+):(\d+)$|^(\d+):(\d+):(\d+):(\d+)$|^(\d+)s$/).optional(), // Validate duration in various formats
     user: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required() // Validate ObjectId string
   });
   return schema.validate(playList);
